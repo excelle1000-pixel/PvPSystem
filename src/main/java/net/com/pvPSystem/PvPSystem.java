66d69c22f442423f29fcd82d1d3c10ac6288f23a
excelle1000-pixel <excelle1000@gmail.com>
2026-04-21 17:52:12 -0600

    s

---
+++ src/main/java/net/com/pvPSystem/PvPSystem.java
@@ -0,0 +1,17 @@
+package net.com.pvPSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PvPSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
